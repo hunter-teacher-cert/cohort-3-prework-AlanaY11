45cd50357895b5b8c3cb5196bfa414e93f7ad559
Alana Robinson <AlanaY11@users.noreply.github.com>
2022-03-11 00:39:32 +0000

    updated

---
+++ pre02/Time.java
@@ -0,0 +1,24 @@
+
+
+public class Time {
+  public static void main (String[] args) {
+
+  //Time.java 4: error missing method body, or declare abstract public static void main (String[] args); {
+  String firstLine = "It's Time to check...";
+  System.out.println(firstLine);
+
+  //Exercise 3 Chap 2.12 - part 2 create variables hour, minute, second
+  int hour = 12;
+  int minute = 59;
+  int second = 30;
+
+//Exercise 3 Chap 2.12 - part 3 make program calculate and display number of seconds since midnight
+
+
+
+  }
+
+  
+  
+}
+
