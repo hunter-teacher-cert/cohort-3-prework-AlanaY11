28ad83064a490db303befc97351a0738b98f5c5d
Alana Robinson <AlanaY11@users.noreply.github.com>
2022-04-14 23:23:34 +0000

    Updated Exercise3.Triangle.java

--- pre05/Exercise3.Triangle.java
+++ pre05/Exercise3.Triangle.java
@@ -1,7 +1,7 @@
 
 import java.util.*;
 
-//Chapter 6 Value Methods Exercise 3 Correct Answer 
+//Chapter 6 Value Methods Exercise 3 
 //If you are given three sticks, you may or may not be able to arrange them in a triangle. For example, if one of the sticks is 12 inches long and the other two are one inch long, you will not be able to get the short sticks to meet in the middle. For any three lengths, there is a simple test to see if it is possible to form a triangle:
 
 //If any of the three lengths is greater than the sum of the other two, you cannot form a triangle.
